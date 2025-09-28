f73fd006a5da721478f3ade83656f80f3dd25d81
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-09-28 16:38:19 +0300

    feat: adding axios and adding api&socket

---
+++ front-end/live-blogging/src/services/socket.js
@@ -0,0 +1,66 @@
+import { io } from 'socket.io-client'
+
+class SocketService {
+  constructor() {
+    this.socket = null
+  }
+
+  connect(token) {
+    if (!this.socket) {
+      this.socket = io('http://localhost:3000', {
+        auth: {
+          token: token
+        }
+      })
+
+      this.socket.on('connect', () => {
+        console.log('Connected to server')
+      })
+
+      this.socket.on('disconnect', () => {
+        console.log('Disconnected from server')
+      })
+    }
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect()
+      this.socket = null
+    }
+  }
+
+  // استماع للأحداث
+  onNewPost(callback) {
+    if (this.socket) {
+      this.socket.on('newPost', callback)
+    }
+  }
+
+  onNewComment(callback) {
+    if (this.socket) {
+      this.socket.on('newComment', callback)
+    }
+  }
+
+  onPostLiked(callback) {
+    if (this.socket) {
+      this.socket.on('postLiked', callback)
+    }
+  }
+
+  onPostDeleted(callback) {
+    if (this.socket) {
+      this.socket.on('postDeleted', callback)
+    }
+  }
+
+  // إزالة المستمعين
+  removeListener(event) {
+    if (this.socket) {
+      this.socket.off(event)
+    }
+  }
+}
+
+export default new SocketService()
